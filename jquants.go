fa320df4c14264b9da8bd748b5c63e5523bf093c
Nicolas Modrzyk <hellonico@gmail.com>
2022-10-03 15:42:32 +0900

    multiple daily quotes

--- jquants.go
+++ jquants.go
@@ -13,6 +13,8 @@ import (
 func main() {
 	code := flag.String("code", "86970", "Company Code")
 	date := flag.String("date", "20220930", "Date of the quote")
+	from := flag.String("from", "20220930", "Start Date for date range")
+	to := flag.String("to", "20220930", "End Date for date range")
 	flag.Parse()
 
 	fmt.Printf("Code: %s and Date: %s\n", *code, *date)
@@ -35,7 +37,14 @@ func main() {
 	edn.Unmarshal(s2, &idtoken)
 	//fmt.Printf("%s\n", idtoken.IdToken)
 
-	url := fmt.Sprintf("https://api.jpx-jquants.com/v1/prices/daily_quotes?code=%s&date=%s", *code, *date)
+	baseUrl := fmt.Sprintf("https://api.jpx-jquants.com/v1/prices/daily_quotes?code=%s", *code)
+	var url string
+	if *from != "" && *to != "" {
+		url = fmt.Sprintf("%s&from=%s&to=%s", baseUrl, *from, *to)
+	} else {
+		url = fmt.Sprintf("%s&date=%s", baseUrl, *date)
+	}
+	fmt.Printf("URL: %s\n", url)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	helper.Check(err)
 
@@ -50,6 +59,9 @@ func main() {
 	err_ := json.NewDecoder(res.Body).Decode(&quotes)
 	helper.Check(err_)
 
-	hello := quotes.DailyQuotes[0]
-	fmt.Printf("Daily: %s > %f\n", hello.Date.String(), hello.Close)
+	fmt.Printf("[%d] Daily Quotes for %s \n", len(quotes.DailyQuotes), *code)
+	for _, quote := range quotes.DailyQuotes {
+		fmt.Printf("%s,%f\n", quote.Date, quote.Close)
+	}
+
 }
